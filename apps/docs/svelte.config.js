f5c2de7927ea60cccec717eab0bcfed32e7fa30d
Nanang Mahdaen El Agung <mahdaen@gmail.com>
2025-03-08 19:51:57 +0700

    Setup workflows

--- apps/docs/svelte.config.js
+++ apps/docs/svelte.config.js
@@ -1,12 +1,17 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { svem } from 'svem';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess(), svem()],
 
 	kit: {
+		paths: {
+			base: dev ? '' : '/svem'
+		},
 		adapter: adapter()
 	}
 };
